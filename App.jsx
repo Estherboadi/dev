b0463ccb96b66b606c646b9748669ad0dc4d9b88
Estherboadi <154574330+Estherboadi@users.noreply.github.com>
2024-06-05 02:43:17 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+
+
+import Header from './Header.jsx'
+
+function App() {
+  return(
+    <div>
+    <Header />
+    </div>
+
+  );
+}
+
+export default App 
+
+
+
+
+
---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+    return(
+        <Header>
+            <div>
+            <img src="/my-react-app/src/assets/react.svg"></img>
+            <h1> My Website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+            </div>
+            
+        </Header>
+
+    );
+}
+export default Header
